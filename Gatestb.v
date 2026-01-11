4e89578a84bfd89fdd5b1713e5635b4a41253aec
Mallah Tejas Rajkumar <230140111020@gtu.edu.in>
2026-01-11 10:32:11 +0530

    Update Gatestb.v

--- Gatestb.v
+++ Gatestb.v
@@ -5,16 +5,20 @@ module test_gates;
     wire and_out, or_out, nand_out, nor_out, xor_out, xnor_out, not_out;
 
     // Instantiate gates with DUT (no underscores) and positional port connection
-    and_gate  DUT(a, b, and_out);
-    or_gate   DUT(a, b, or_out);
-    nand_gate DUT(a, b, nand_out);
-    nor_gate  DUT(a, b, nor_out);
-    xor_gate  DUT(a, b, xor_out);
-    xnor_gate DUT(a, b, xnor_out);
-    not_gate  DUT(a, not_out);
+    and_gate  u1(a, b, and_out);
+    or_gate   u2(a, b, or_out);
+    nand_gate u3(a, b, nand_out);
+    nor_gate  u4(a, b, nor_out);
+    xor_gate  u5(a, b, xor_out);
+    xnor_gate u6(a, b, xnor_out);
+    not_gate  u7(a, not_out);
+
 
     initial begin
         $display("A B | AND OR NAND NOR XOR XNOR NOT");
+        $dumpfile("gates.vcd");
+        $dumpvars(0, test_gates);
+
         for (integer i = 0; i < 4; i = i + 1) begin
             a = i[1];
             b = i[0];
